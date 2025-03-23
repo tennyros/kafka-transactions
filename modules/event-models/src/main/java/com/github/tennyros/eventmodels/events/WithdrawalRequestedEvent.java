d35e7a517d5ab587ac111e6cf9298a4626fee406
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-03-23 11:51:13 +0500

    fix(KT-1): add @RequestBody to TransferRestModel in transfer-service, add @ToString to events in event-models (#2)
    - add @RequestBody to TransferRestModel in transfer-service;
    - add @ToString to events in event-models";
    - test withdraw-deposit topics:
    
    ![postman](https://github.com/user-attachments/assets/e19401f8-f945-4bbe-9390-71adb06c3f30)
    
    ![spring-boot-logs](https://github.com/user-attachments/assets/c9222cca-1e0f-4b94-9a14-3d0f044a43c8)
    
    ![consumer-deposit-topic](https://github.com/user-attachments/assets/1dac9d4a-ae1f-4e04-b914-83521511e05d)
    
    ![consumer-withdraw-topic](https://github.com/user-attachments/assets/68ffa500-8530-476d-a827-3dbddefd7cf7)

--- modules/event-models/src/main/java/com/github/tennyros/eventmodels/events/WithdrawalRequestedEvent.java
+++ modules/event-models/src/main/java/com/github/tennyros/eventmodels/events/WithdrawalRequestedEvent.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class WithdrawalRequestedEvent {
--- modules/transfer-service/src/main/java/com/github/tennyros/transferservice/rest/TransfersController.java
+++ modules/transfer-service/src/main/java/com/github/tennyros/transferservice/rest/TransfersController.java
@@ -5,6 +5,7 @@ import com.github.tennyros.transferservice.service.TransferService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class TransfersController {
     private final TransferService transferService;
 
     @PostMapping
-    public boolean transfer(TransferRestModel transferRestModel) {
+    public boolean transfer(@RequestBody TransferRestModel transferRestModel) {
         return transferService.transfer(transferRestModel);
     }
 
